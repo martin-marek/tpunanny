c641ae65d7691eb9ca3f7eef19432780523dd9b4
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2025-09-21 19:05:55 -0400

    Create tpu_monitor.py

---
+++ tpu_monitor.py
@@ -0,0 +1,50 @@
+import sys
+import time
+from google.cloud import tpu_v2
+from datetime import datetime, timedelta
+from dateutil import parser
+import fire
+from rich.live import Live
+from rich.table import Table
+client = tpu_v2.TpuClient()
+
+def generate_tpu_table(project_id):
+    """Fetches, sorts, and builds a rich Table of TPUs."""
+
+    # create table
+    table = Table()
+    for header in ['ID', 'Zone', 'Type', 'Spot', 'State', 'Created', 'Uptime']:
+        table.add_column(header)
+    table.caption = f'Last updated: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}'
+    
+    # add nodes to table
+    nodes = client.list_nodes(parent=f'projects/{project_id}/locations/-')
+    for node in sorted(nodes, key=lambda x: x.create_time):
+        time_created = node.create_time
+        time_now = datetime.now(time_created.tzinfo)
+        time_last = time_now if not 'maintenance' in node.health_description else parser.parse(node.health_description, fuzzy=True)
+        table.add_row(
+            node.name.split('/')[-1], # ID
+            node.name.split('/')[3], # zone
+            node.accelerator_type, # accelerator type
+            '✅' if node.scheduling_config.spot else '❌', # spot
+            node.state.name, # state
+            str(timedelta(seconds=round((time_now - time_created).total_seconds()))), # created
+            str(timedelta(seconds=round((time_last - time_created).total_seconds()))), # uptime
+            style={'READY': 'dark_green', 'CREATING': 'yellow', 'PREEMPTED': 'red'}[node.state.name] # color
+        )
+    return table
+
+def run_monitor(project_id):
+    """Monitors TPUs in a GCP project with a live-updating table."""
+    try:
+        with Live(generate_tpu_table(project_id), screen=True, auto_refresh=False) as live:
+            while True:
+                time.sleep(2)
+                live.update(generate_tpu_table(project_id), refresh=True)
+    except KeyboardInterrupt:
+        print('\n👋 Monitoring stopped.')
+        sys.exit(0)
+
+if __name__ == '__main__':
+    fire.Fire(run_monitor)
