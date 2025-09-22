c90ca151e4891a2a3380d19d19c8e2853ef9ac42
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2025-09-21 22:26:23 -0400

    Update tpu_monitor.py

--- tpu_monitor.py
+++ tpu_monitor.py
@@ -1,17 +1,17 @@
 import sys
 import time
+import fire
 from google.cloud import tpu_v2
 from datetime import datetime, timedelta
 from dateutil import parser
-import fire
 from rich.live import Live
 from rich.table import Table
 client = tpu_v2.TpuClient()
 
-def generate_tpu_table(project_id):
-    """Fetches, sorts, and builds a rich Table of TPUs."""
 
-    # create table
+def generate_tpu_table(project_id):
+
+    # create empty table
     table = Table()
     for header in ['ID', 'Zone', 'Type', 'Schedule', 'State', 'Created', 'Uptime']:
         table.add_column(header)
@@ -29,22 +29,25 @@ def generate_tpu_table(project_id):
             node.accelerator_type, # accelerator type
             'spot' if node.scheduling_config.spot else 'on-demand', # spot
             node.state.name, # state
-            str(timedelta(seconds=round((time_now - time_created).total_seconds()))), # created
-            str(timedelta(seconds=round((time_last - time_created).total_seconds()))), # uptime
+            str(timedelta(seconds=round((time_now - time_created).total_seconds()))).split(',')[0], # created
+            str(timedelta(seconds=round((time_last - time_created).total_seconds()))).split(',')[0], # uptime
             style={'READY': 'dark_green', 'CREATING': 'dark_orange', 'PREEMPTED': 'dark_red'}[node.state.name] # color
         )
+
     return table
 
-def run_monitor(project_id):
+
+def run_monitor(project_id, interval=1):
     """Monitors TPUs in a GCP project with a live-updating table."""
     try:
         with Live(generate_tpu_table(project_id), screen=True, auto_refresh=False) as live:
             while True:
-                time.sleep(2)
+                time.sleep(interval)
                 live.update(generate_tpu_table(project_id), refresh=True)
     except KeyboardInterrupt:
         print('\n👋 Monitoring stopped.')
         sys.exit(0)
+
 
 if __name__ == '__main__':
     fire.Fire(run_monitor)
